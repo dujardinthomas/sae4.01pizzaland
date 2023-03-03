afb6ef50db62f7dcad6c4366266472faad2840be
Thomas Dujardin <thomas.dujardin2.etu@univ-lille.fr>
2023-03-03 23:39:45 +0100

    commande maj nom colonne

--- WEB-INF/src/dao/CommandeDAO.java
+++ WEB-INF/src/dao/CommandeDAO.java
@@ -43,7 +43,7 @@ public class CommandeDAO {
 			ResultSet rs = stmt.executeQuery(query);
 			while(rs.next()){
 				int idC = rs.getInt("idCo");
-				commandes.add(new Commande(idC, rs.getInt("client_id"), rs.getString("date"), commande_pizzDAO.getCommandePizza(idC)));
+				commandes.add(new Commande(idC, rs.getInt("client_id"), rs.getString("dateco"), commande_pizzDAO.getCommandePizza(idC)));
 			}
 			return commandes;
 		}
@@ -56,7 +56,7 @@ public class CommandeDAO {
 			ResultSet rs = stmt.executeQuery(query);
 			while(rs.next()){
 				int idC = rs.getInt("idCo");
-				c = new Commande(idC, rs.getInt("client_id"), rs.getString("date"), commande_pizzDAO.getCommandePizza(idC));
+				c = new Commande(idC, rs.getInt("client_id"), rs.getString("dateco"), commande_pizzDAO.getCommandePizza(idC));
 			}
 			return c;
 		}
